d38ea6de1d8ae9ed9fb9ef979bd4685789ab1c17
Sergio Ramirez Artavia <30870567+devsergioraar@users.noreply.github.com>
2024-04-15 00:05:51 -0600

    Proof of Concept
    Upload a file into a Personal Onedrive

---
+++ LoadFile.py
@@ -0,0 +1,28 @@
+import os
+import requests
+import json
+from ms_graph import generate_access_token, GRAPH_API_ENDPOINT
+
+APP_ID='284e3dec-696b-4a19-9ea6-1145463774de'
+SCOPES=['Files.ReadWrite.All']
+
+access_token = generate_access_token(APP_ID, SCOPES)
+ 
+headers = {
+    'Authorization' : 'Bearer ' + access_token['access_token']
+}
+
+file_path = 'C:\\Users\\Usuario\\OneDrive\\ProCanDis by Sergio v0.1\\Manual llave criptográfica.pdf'
+file_name = os.path.basename(file_path)
+
+with open(file_path, 'rb') as upload:
+    media_content = upload.read()
+
+#upload file
+response = requests.put(
+    GRAPH_API_ENDPOINT + f'/me/drive/items/root:/{file_name}:/content',
+    headers=headers,
+    data=media_content
+)
+val = str(response.json())
+print(val)  
